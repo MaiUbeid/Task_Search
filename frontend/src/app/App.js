a3c49a1bc10da0f3bf0c8e7c4e2774e229a100d6
Mai Ubeid <mai@pop-os.localdomain>
2020-07-23 16:56:06 +0300

    setup the react and graohQL (#1) #1

---
+++ frontend/src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
