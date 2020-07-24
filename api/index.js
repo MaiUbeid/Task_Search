78d621fc5adfad2c1fad3e1698b8757535a0fd5b
Mai Ubeid <mai@pop-os.localdomain>
2020-07-24 21:49:46 +0300

    save the object on json file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,48 @@
 const { ApolloServer, gql } = require('apollo-server');
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const queryString = require('querystring');
 
 const { allCategories } = require('./categories.json');
+
+const addCategoryJson = () => {
+  // data will be an argument from api
+  const data = {
+    id: allCategories.length + 1,
+    title: 'write',
+    keywords: [
+      'publish',
+      'compose',
+      'pen',
+      'spell',
+      'drop',
+      'a line',
+      'indite',
+      'read',
+      'rewrite',
+      'submit',
+    ],
+  };
+
+  const convertedData = queryString.parse(allCategories);
+  const filePath = path.join(__dirname, 'categories.json');
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'application/javascript' });
+      response.end();
+    } else {
+      const categories = JSON.parse(file);
+      const fileData = convertedData;
+      console.log(fileData);
+      categories.allCategories.push(data);
+      console.log(categories);
+      fs.writeFile(filePath, JSON.stringify(categories), (err) =>
+        console.log(err)
+      );
+    }
+  });
+};
 
 const typeDefs = gql`
   type Category {
@@ -30,19 +71,17 @@ const resolvers = {
 
     postCategory: async (_parent, args) => {
       const { category } = args;
-      console.log('mai');
       try {
         const keywords = await axios.get(
           `https://api.datamuse.com/words?ml=${category}`
         );
 
-        console.log(keywords);
+        addCategoryJson(keywords);
 
-        return keywords.data.slice(0, 9).map((item) => {
-          return item;
-        });
+        // return keywords.data.slice(0, 9).map((item) => {
+        //   return item;
+        // });
       } catch (error) {
-        console.log(error);
         throw error;
       }
     },
