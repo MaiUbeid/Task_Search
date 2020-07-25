974260b4c2fb30603015c67fffaa7f811e131c4e
Mai Ubeid <mai@pop-os.localdomain>
2020-07-25 19:29:13 +0300

    fixing

--- api/index.js
+++ api/index.js
@@ -7,19 +7,16 @@ const queryString = require('querystring');
 const { allCategories } = require('./categories.json');
 
 const addCategoryJson = (newCategory) => {
-  const convertedData = queryString.parse(allCategories);
   const filePath = path.join(__dirname, 'categories.json');
-  fs.readFileSync(filePath, (error, file) => {
+
+  fs.readFile(filePath, (error, file) => {
     if (error) {
-      response.writeHead(500, { 'Content-Type': 'application/javascript' }); // there is no response object here !!
+      response.writeHead(500, { 'Content-Type': 'application/javascript' });
       response.end();
     } else {
       const categories = JSON.parse(file);
-      const fileData = convertedData;
-      console.log(fileData);
       categories.allCategories.push(newCategory);
-      console.log(categories);
-      fs.writeFileSync(filePath, JSON.stringify(categories), (err) =>
+      fs.writeFile(filePath, JSON.stringify(categories), (err) =>
         console.log(err)
       );
     }
@@ -68,7 +65,7 @@ const resolvers = {
             .slice(0, 3),
         };
 
-        await addCategoryJson(newCategory);
+        addCategoryJson(newCategory);
 
         return keywords.data.slice(0, 9).map((item) => {
           return item;
